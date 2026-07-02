fe769c787b4aee1570825ec08bf012923e7ad170
Turab Manzoor <turabmanzoor032@gmail.com>
2026-07-02 13:08:12 +0530

    stan & normalization && confusion matrix

--- logistic_regression.py
+++ logistic_regression.py
@@ -30,3 +30,25 @@ y_pred = model.predict(x_test)
 print("Accuracy=",accuracy_score(y_test,y_pred)*100,"%")
 print("precison=",precision_score(y_test,y_pred)*100,"%")
 # %%
+from sklearn.preprocessing import StandardScaler
+
+
+# %%
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+# %%
+model.fit(x_train,y_train)
+# %%
+y_pred = model.predict(x_test)
+
+# %%
+
+print("Accuracy:",accuracy_score(y_test,y_pred)*100,"%")
+print("Precision:",precision_score(y_test,y_pred)*100,"%")
+# %%
+from sklearn.metrics import confusion_matrix
+# %%
+confusion_matrix(y_test,y_pred)
+# %%
